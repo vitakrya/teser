7bb8f70a8d7f7add67eb14e566905519cc35eb9e
Rushan <trlearning@mail.ru>
2016-05-23 00:19:30 +0300

    Add ts thing

--- js/firstTest.js
+++
@@ -1,8 +0,0 @@
-function sortByName(a) {
-    var result = a.slice(0);
-    result.sort(function (x, y) {
-        return x.name.localeCompare(y.name);
-    });
-    return result;
-}
-sortByName([]);
